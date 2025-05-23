9a1d00a0b3cdc6a8723d801e7f9494f6f1a68cba
Binayak Dotel <binayak01.bd@gmail.com>
2025-05-23 09:52:44 +0545

    Action fixes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Remove basePath as it might be causing the 404 issue
+  // We'll handle routing through GitHub Pages configuration
 };
 
 module.exports = nextConfig; 
